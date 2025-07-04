bc62f054634f2135c4457a521731fc06f64362a5
yeonzeop <ksp2617@naver.com>
2025-07-04 16:27:31 +0900

    [Feat] #5: BuildingService에서 findBuildingById 구현

--- src/main/java/com/example/BarrierKU/service/BuildingService.java
+++ src/main/java/com/example/BarrierKU/service/BuildingService.java
@@ -1,8 +1,17 @@
 package com.example.BarrierKU.service;
 
+import com.example.BarrierKU.domain.Indoor.Building;
+import com.example.BarrierKU.repository.BuildingRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BuildingService {
+    private final BuildingRepository buildingRepository;
 
+    public Building findBuildingById(Long id) {
+        return buildingRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+    }
 }
