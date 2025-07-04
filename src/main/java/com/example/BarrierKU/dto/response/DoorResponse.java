1ca4ec6e76f238b388b7faa06078b233be9d7f73
yeonzeop <ksp2617@naver.com>
2025-07-04 17:12:06 +0900

    [Feat] #5: DoorResponse 구현

---
+++ src/main/java/com/example/BarrierKU/dto/response/DoorResponse.java
@@ -0,0 +1,25 @@
+package com.example.BarrierKU.dto.response;
+
+import com.example.BarrierKU.domain.Image.DoorImage;
+import com.example.BarrierKU.domain.Indoor.Door;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class DoorResponse {
+    private Long id;
+
+    private boolean wheelchair;
+
+    private String significant;
+
+    private List<DoorImage> images;
+
+    public static DoorResponse from(Door door) {
+        return new DoorResponse(door.getId(), door.getWheelchair(), door.getSignificant(), door.getImages());
+    }
+
+}
