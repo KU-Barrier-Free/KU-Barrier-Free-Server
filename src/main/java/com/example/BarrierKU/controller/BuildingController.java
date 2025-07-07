647103847cf28a00092259e2d63fe431e4bf3fd6
yeonzeop <ksp2617@naver.com>
2025-07-07 10:52:25 +0900

    [Feat] #5: BuildingController 조회 메서드 구현

---
+++ src/main/java/com/example/BarrierKU/controller/BuildingController.java
@@ -0,0 +1,25 @@
+package com.example.BarrierKU.controller;
+
+import com.example.BarrierKU.domain.Indoor.Building;
+import com.example.BarrierKU.dto.response.BuildingResponse;
+import com.example.BarrierKU.service.BuildingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/buildings")
+@RequiredArgsConstructor
+public class BuildingController {
+    private final BuildingService buildingService;
+
+    @GetMapping("/{buildingId}")
+    public ApiResponse<BuildingResponse> getBuilding(@PathVariable Long buildingId){
+        Building building = buildingService.findBuildingById(buildingId);
+        return ApiResponse.success(BuildingResponse.from(building));
+    }
+
+
+}
