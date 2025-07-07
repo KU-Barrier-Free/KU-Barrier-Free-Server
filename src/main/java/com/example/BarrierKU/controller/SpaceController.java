1cc3a3322c7836ef403a13a5cc65fa09f0996d7b
choihongjun <vvclsbalam@gmail.com>
2025-07-07 12:52:23 +0900

    [Feat] #6: SpaceController 건물 상세 조회 요청 메소드 구현

--- src/main/java/com/example/BarrierKU/controller/SpaceController.java
+++ src/main/java/com/example/BarrierKU/controller/SpaceController.java
@@ -1,14 +1,25 @@
 package com.example.BarrierKU.controller;
 
+import com.example.BarrierKU.dto.SpaceResponse;
 import com.example.BarrierKU.service.SpaceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("buildings/")
+@RestController
+@RequestMapping("buildings/{buildingId}")
 @RequiredArgsConstructor
 public class SpaceController {
 
     private final SpaceService spaceService;
+
+    @GetMapping("/spaces/{spaceId}")
+    public ResponseEntity<SpaceResponse> getSpaceDetail(
+            @PathVariable Long buildingId,
+            @PathVariable Long spaceId,
+            @RequestParam int type
+    ) {
+        SpaceResponse response = spaceService.getResponse(spaceId, type);
+        return ResponseEntity.ok(response);
+    }
 }
