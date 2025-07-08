2d1704b4c07c65d4256af7950c72164ad165e717
yeonzeop <ksp2617@naver.com>
2025-07-08 15:15:17 +0900

    [Feat] #5: ApiResponse Class 생성 및 success 메서드

---
+++ src/main/java/com/example/BarrierKU/dto/response/ApiResponse.java
@@ -0,0 +1,17 @@
+package com.example.BarrierKU.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
+
+public record ApiResponse<T>(
+        @JsonIgnore
+        HttpStatus httpStatus,
+        boolean success,
+        @Nullable T data,
+        @Nullable ExceptionDTO error
+) {
+    public static <T> ApiResponse<T> success(@Nullable final T data){
+        return new ApiResponse<>(HttpStatus.OK, true, data, null);
+    }
+}
