ce8cf43f0828c8649f5624be7f38d713bc11b7c0
yeonzeop <ksp2617@naver.com>
2025-07-08 18:52:51 +0900

    [Refactor] #5: 응답 코드 수정

--- src/main/java/com/example/BarrierKU/common/response/ResponseCode.java
+++ src/main/java/com/example/BarrierKU/common/response/ResponseCode.java
@@ -15,15 +15,16 @@ import lombok.NoArgsConstructor;
 public enum ResponseCode {
 
     // 1000 번대 : global 요청 성공/실패
-    SUCCESS(true, 1000, "요청에 성공하였습니다."),
-    INVALID_PATH_VARIABLE_TYPE(false, 1001, "요청 경로에 포함된 값의 타입이 올바르지 않습니다. 올바른 형식으로 요청해주세요."),
+    SUCCESS(true, 200, "요청에 성공하였습니다."),
+    INVALID_PATH_VARIABLE_TYPE(false, 400, "요청 경로에 포함된 값의 타입이 올바르지 않습니다. 올바른 형식으로 요청해주세요."),
 
     // 2000 번대 : home
 
     // 3000 번대 : building
+    BUILDING_NOT_FOUND(false, 404, "해당 건물을 찾을 수 없습니다."),
 
     // 4000 번대 : facility
-    FACILITY_NOT_FOUND(false, 4001, "해당 편의시설을 찾을 수 없습니다.");
+    FACILITY_NOT_FOUND(false, 404, "해당 편의시설을 찾을 수 없습니다.");
 
     // 5000 번대 : place
 
--- src/main/java/com/example/BarrierKU/service/BuildingService.java
+++ src/main/java/com/example/BarrierKU/service/BuildingService.java
@@ -1,5 +1,6 @@
 package com.example.BarrierKU.service;
 
+import com.example.BarrierKU.common.exception.BarrierKuException;
 import com.example.BarrierKU.domain.Indoor.Building;
 import com.example.BarrierKU.dto.response.BuildingResponse;
 import com.example.BarrierKU.repository.BuildingRepository;
@@ -7,13 +8,16 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static com.example.BarrierKU.common.response.ResponseCode.BUILDING_NOT_FOUND;
+
 @Service
 @RequiredArgsConstructor
 public class BuildingService {
     private final BuildingRepository buildingRepository;
 
     public BuildingResponse findBuildingById(Long id) {
-        Building building = buildingRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+        Building building = buildingRepository.findById(id)
+                .orElseThrow(() -> new BarrierKuException(BUILDING_NOT_FOUND));
         return BuildingResponse.from(building);
     }
 }
