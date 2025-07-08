1cbaffb7124e2567dc7d48b76a91eab888e761bc
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-07-08 11:33:27 +0900

    feat: swagger 설정 추가

---
+++ src/main/java/com/example/BarrierKU/domain/facility/controller/FacilityController.java
@@ -0,0 +1,11 @@
+package com.example.BarrierKU.domain.facility.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/facilities")
+@RestController
+public class FacilityController {
+}
