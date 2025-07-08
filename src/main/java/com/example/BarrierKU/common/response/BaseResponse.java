1754e6712b666746a7136ba6beec8e96f3ae0ea2
yeonzeop <ksp2617@naver.com>
2025-07-08 17:40:05 +0900

    [Fix] #5:BaseResponse Class

---
+++ src/main/java/com/example/BarrierKU/common/response/BaseResponse.java
@@ -0,0 +1,36 @@
+package com.example.BarrierKU.common.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+
+@Getter
+public class BaseResponse<T> {
+    @JsonProperty("isSuccess")
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+    private final T result;
+
+    public BaseResponse(ResponseCode responseCode, T result) {
+        this.isSuccess = responseCode.isSuccess();
+        this.code = responseCode.getCode();
+        this.message = responseCode.getMessage();
+        this.result = result;
+    }
+
+    public BaseResponse(ResponseCode responseCode) {
+        this.isSuccess = responseCode.isSuccess();
+        this.code = responseCode.getCode();
+        this.message = responseCode.getMessage();
+        this.result = null;
+    }
+
+    public BaseResponse(T result) {
+        ResponseCode responseCode = ResponseCode.SUCCESS;
+        this.isSuccess = responseCode.isSuccess();
+        this.code = responseCode.getCode();
+        this.message = responseCode.getMessage();
+        this.result = result;
+    }
+}
--- src/main/java/com/example/BarrierKU/controller/BuildingController.java
+++ src/main/java/com/example/BarrierKU/controller/BuildingController.java
@@ -1,27 +1,21 @@
 package com.example.BarrierKU.controller;
 
-import com.example.BarrierKU.domain.Indoor.Building;
-import com.example.BarrierKU.common.ApiResponse;
+import com.example.BarrierKU.common.response.BaseResponse;
 import com.example.BarrierKU.dto.response.BuildingResponse;
 import com.example.BarrierKU.service.BuildingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@ResponseBody
+@RestController
 @RequestMapping("/buildings")
 @RequiredArgsConstructor
 public class BuildingController {
     private final BuildingService buildingService;
 
     @GetMapping("/{buildingId}")
-    public ApiResponse<BuildingResponse> getBuilding(@PathVariable Long buildingId){
+    public BaseResponse<BuildingResponse> getBuilding(@PathVariable Long buildingId){
         BuildingResponse response = buildingService.findBuildingById(buildingId);
-        return ApiResponse.ok(response);
+        return new BaseResponse(response);
     }
 
 
