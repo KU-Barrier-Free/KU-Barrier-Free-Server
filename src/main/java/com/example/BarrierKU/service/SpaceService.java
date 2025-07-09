6bba19c8738add597950e84277679c939809211b
choihongjun <vvclsbalam@gmail.com>
2025-07-09 19:49:29 +0900

    [Refactor] #6: SpaceService에서 orElseThrow로 예외처리

--- src/main/java/com/example/BarrierKU/service/SpaceService.java
+++ src/main/java/com/example/BarrierKU/service/SpaceService.java
@@ -1,11 +1,14 @@
 package com.example.BarrierKU.service;
 
+import com.example.BarrierKU.common.exception.BarrierKuException;
 import com.example.BarrierKU.domain.Indoor.Room;
 import com.example.BarrierKU.dto.SpaceResponse;
 import com.example.BarrierKU.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.BarrierKU.common.response.ResponseCode.SPACE_NOT_FOUND;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,7 +18,8 @@ public class SpaceService {
     private final RoomRepository roomRepository;
 
     public SpaceResponse getResponse(Long spaceId, int type) {
-        Room room = roomRepository.findById(spaceId);
+        Room room = roomRepository.findById(spaceId)
+                .orElseThrow(() -> new BarrierKuException(SPACE_NOT_FOUND));
         return SpaceResponse.of(room, type);
     }
 }
