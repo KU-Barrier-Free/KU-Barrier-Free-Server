5d75d253347358720694d3b2ae12bf3dc5863b5c
ksg <ksg001227@naver.com>
2025-07-09 17:35:01 +0900

    [Feat] #11 Swagger 예외 표시용 Enum 생성

--- src/main/java/com/example/BarrierKU/common/swagger/SwaggerResponseDescription.java
+++ src/main/java/com/example/BarrierKU/common/swagger/SwaggerResponseDescription.java
@@ -1,2 +1,40 @@
-package com.example.BarrierKU.common.annotation;public enum SwaggerResponseDescription {
+package com.example.BarrierKU.common.swagger;
+
+import com.example.BarrierKU.common.response.ResponseCode;
+import lombok.Getter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static com.example.BarrierKU.common.response.ResponseCode.*;
+
+@Getter
+public enum SwaggerResponseDescription {
+
+    TEST(new LinkedHashSet<>(Set.of(
+            TEST_EXCEPTION
+    ))),
+
+    GET_BUILDING(new LinkedHashSet<>(Set.of(
+            BUILDING_NOT_FOUND
+    ))),
+
+    GET_FACILITY(new LinkedHashSet<>(Set.of(
+            FACILITY_NOT_FOUND
+    )));
+
+    private final Set<ResponseCode> responseCodeSet;
+
+    SwaggerResponseDescription(Set<ResponseCode> responseCodeSet) {
+        responseCodeSet.addAll(new LinkedHashSet<>(Set.of(
+                SUCCESS,
+                INVALID_PATH_VARIABLE_TYPE,
+                BAD_REQUEST,
+                API_NOT_FOUND,
+                METHOD_NOT_ALLOWED,
+                INTERNAL_SERVER_ERROR
+        )));
+
+        this.responseCodeSet = responseCodeSet;
+    }
 }
