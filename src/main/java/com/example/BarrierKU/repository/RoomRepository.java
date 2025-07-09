a140a7c51492119d9a815dbf6ee94e71cf3357b9
choihongjun <vvclsbalam@gmail.com>
2025-07-09 19:40:21 +0900

    [Refactor] #6: JpaRepository를 상속해  RoomRepository 구현

--- src/main/java/com/example/BarrierKU/repository/RoomRepository.java
+++ src/main/java/com/example/BarrierKU/repository/RoomRepository.java
@@ -1,34 +1,8 @@
 package com.example.BarrierKU.repository;
 
 import com.example.BarrierKU.domain.Indoor.Room;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+public interface RoomRepository extends JpaRepository<Room, Long> {
 
-@Repository
-@RequiredArgsConstructor
-public class RoomRepository {
-
-    private final EntityManager em;
-
-    public void save(Room room) {
-        em.persist(room);
-    }
-
-    public Room findById(Long id) {
-        return em.find(Room.class, id);
-    }
-
-    public List<Room> findAll() {
-        return em.createQuery("select r from Room r", Room.class)
-                .getResultList();
-    }
-
-    public List<Room> findByName(String name) {
-        return em.createQuery("select r from Room r where r.roomName = :name", Room.class)
-                .setParameter("name", name) // 쿼리 name 설정
-                .getResultList();
-    }
 }
