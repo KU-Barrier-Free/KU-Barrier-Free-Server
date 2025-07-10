b328b39b92c348224ebfe3e4192cbd3976e476f1
ksg <ksg001227@naver.com>
2025-07-10 12:47:22 +0900

    [Fix] #13 HomeLocationResponse -> HomeItem 으로 이름 변경

--- src/main/java/com/example/BarrierKU/domain/home/dto/HomeItem.java
+++ src/main/java/com/example/BarrierKU/domain/home/dto/HomeItem.java
@@ -2,7 +2,7 @@ package com.example.BarrierKU.domain.home.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record HomeLocationResponse(
+public record HomeItem(
         @Schema(description = "각 항목들의 PK값", example = "1")
         Long id,
         @Schema(description = "위도", example = "37.123456")
@@ -10,8 +10,8 @@ public record HomeLocationResponse(
         @Schema(description = "경도", example = "127.123456")
         double longitude
 ) {
-    public static HomeLocationResponse of(Long id, double latitude, double longitude) {
-        return new HomeLocationResponse(
+    public static HomeItem of(Long id, double latitude, double longitude) {
+        return new HomeItem(
                 id,
                 latitude,
                 longitude
--- src/main/java/com/example/BarrierKU/domain/home/dto/HomeResponse.java
+++ src/main/java/com/example/BarrierKU/domain/home/dto/HomeResponse.java
@@ -6,21 +6,21 @@ import java.util.List;
 
 public record HomeResponse(
         @Schema(description = "건물들 정보")
-        List<HomeLocationResponse> buildings,
+        List<HomeItem> buildings,
         @Schema(description = "특이사항들 정보")
-        List<HomeLocationResponse> significants,
+        List<HomeItem> significants,
         @Schema(description = "연석들 정보")
-        List<HomeLocationResponse> curbs,
+        List<HomeItem> curbs,
         @Schema(description = "경사로들 정보")
-        List<HomeLocationResponse> ramps,
+        List<HomeItem> ramps,
         @Schema(description = "계단들 정보")
-        List<HomeLocationResponse> stairs
+        List<HomeItem> stairs
 ) {
-    public static HomeResponse of(List<HomeLocationResponse> buildings,
-                                  List<HomeLocationResponse> significants,
-                                  List<HomeLocationResponse> curbs,
-                                  List<HomeLocationResponse> ramps,
-                                  List<HomeLocationResponse> stairs) {
+    public static HomeResponse of(List<HomeItem> buildings,
+                                  List<HomeItem> significants,
+                                  List<HomeItem> curbs,
+                                  List<HomeItem> ramps,
+                                  List<HomeItem> stairs) {
         return new HomeResponse(
                 buildings,
                 significants,
