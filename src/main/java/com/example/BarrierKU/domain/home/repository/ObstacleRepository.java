27e2a55610a04ae0f6fd92b1cda04ea26ec934a5
ksg <ksg001227@naver.com>
2025-07-10 11:57:01 +0900

    [Feat] #13 ObstacleRepository 생성

---
+++ src/main/java/com/example/BarrierKU/domain/home/repository/ObstacleRepository.java
@@ -0,0 +1,7 @@
+package com.example.BarrierKU.domain.home.repository;
+
+import com.example.BarrierKU.domain.Outdoor.Obstacle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ObstacleRepository extends JpaRepository<Obstacle, Long> {
+}
