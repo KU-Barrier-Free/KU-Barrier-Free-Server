adbb09aa6d0ce25db6310aeec3c5d8ded56250c1
yeonzeop <ksp2617@naver.com>
2025-07-14 19:55:20 +0900

    [Feat] #17: BuildingController에 getFloorSpaces 추가

--- src/main/java/com/example/BarrierKU/domain/building/controller/BuildingController.java
+++ src/main/java/com/example/BarrierKU/domain/building/controller/BuildingController.java
@@ -4,6 +4,8 @@ import com.example.BarrierKU.common.annotation.CustomExceptionDescription;
 import com.example.BarrierKU.common.response.BaseResponse;
 import com.example.BarrierKU.domain.building.dto.BuildingResponse;
 import com.example.BarrierKU.domain.building.service.BuildingService;
+import com.example.BarrierKU.domain.floor.dto.FloorResponse;
+import com.example.BarrierKU.domain.floor.service.FloorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,7 @@ import static com.example.BarrierKU.common.swagger.SwaggerResponseDescription.GE
 @RequiredArgsConstructor
 public class BuildingController {
     private final BuildingService buildingService;
+    private final FloorService floorService;
 
     @GetMapping("/{buildingId}")
     @CustomExceptionDescription(GET_BUILDING)
@@ -22,5 +25,13 @@ public class BuildingController {
         return BaseResponse.ok(response);
     }
 
+    @GetMapping("/{buildingId}/spaces")
+    public BaseResponse<FloorResponse> getFloorSpaces(
+            @PathVariable Long buildingId,
+            @RequestParam String floor
+    ) {
+        FloorResponse response = floorService.getFloorInfo(buildingId, floor);
+        return BaseResponse.ok(response);
+    }
 
 }
