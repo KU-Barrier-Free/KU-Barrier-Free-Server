1289e19e263c185d68ae3b0528451ccb226becac
yeonzeop <ksp2617@naver.com>
2025-07-15 16:40:22 +0900

    [Feat] #17: getFloorSpaces 메소드에 예외 어노테이션 추가

--- src/main/java/com/example/BarrierKU/domain/building/controller/BuildingController.java
+++ src/main/java/com/example/BarrierKU/domain/building/controller/BuildingController.java
@@ -26,6 +26,7 @@ public class BuildingController {
     }
 
     @GetMapping("/{buildingId}/spaces")
+    @CustomExceptionDescription(GET_BUILDING)
     public BaseResponse<FloorResponse> getFloorSpaces(
             @PathVariable Long buildingId,
             @RequestParam String floor
