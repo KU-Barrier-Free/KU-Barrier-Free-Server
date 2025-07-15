689bb554cc2fa0260573a294c47dc4840177d2b5
yeonzeop <ksp2617@naver.com>
2025-07-15 17:09:40 +0900

    [Refactor] #17: Schema example 작성

--- src/main/java/com/example/BarrierKU/domain/building/dto/BuildingResponse.java
+++ src/main/java/com/example/BarrierKU/domain/building/dto/BuildingResponse.java
@@ -6,6 +6,7 @@ import com.example.BarrierKU.domain.indoor.Building;
 import com.example.BarrierKU.domain.indoor.Door;
 import com.example.BarrierKU.domain.indoor.Significant;
 import com.example.BarrierKU.domain.type.Purpose;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -17,13 +18,43 @@ import java.util.stream.Collectors;
 @Getter
 @AllArgsConstructor
 public class BuildingResponse {
+    @Schema(description = "건물 아이디", example = "1")
     private Long id;
+    @Schema(description = "건물 번호", example = "2")
     private int number;
+    @Schema(description = "건물명", example = "경영관")
     private String name;
+    @Schema(description = "건물 담당 부서", example = "경영(전문)대학원, 경영대학")
     private String department;
+    @Schema(description = "도면 이미지", example = "image.png")
     private String image;
+    @Schema(
+            description = "문 정보 리스트",
+            example = "[{" +
+                    "\"id\": 1," +
+                    "\"wheelchair\": true," +
+                    "\"imageUrl\": [\"https://example.com/door1.jpg\", \"https://example.com/door2.jpg\"]" +
+                    "}, {" +
+                    "\"id\": 2," +
+                    "\"wheelchair\": false," +
+                    "\"imageUrl\": [\"https://example.com/door3.jpg\"]" +
+                    "}]"
+    )
     private List<DoorInfo> doorInfos;
+    @Schema(description = "해당 건물에 존재하는 편의시설 종류", example = "[\"은행\", \"휴게실\", \"카페\"]")
     private Set<String> facilityPurposes;
+    @Schema(
+            description = "특이사항 리스트",
+            example = "[{" +
+                    "\"id\": 1," +
+                    "\"description\": \"경사로\"," +
+                    "\"imageUrl\": [\"https://example.com/significant1.jpg\"]" +
+                    "}, {" +
+                    "\"id\": 2," +
+                    "\"description\": \"계단으로도 이용 가능\"," +
+                    "\"imageUrl\": [\"https://example.com/significant2.jpg\", \"https://example.com/significant3.jpg\"]" +
+                    "}]"
+    )
     private List<SignificantInfo> significantInfos;
 
     @Getter
