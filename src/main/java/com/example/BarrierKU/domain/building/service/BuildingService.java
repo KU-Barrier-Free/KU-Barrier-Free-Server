ca730ae2e3d70a7747013c00e301a9a3f7cd2bd2
choihongjun <vvclsbalam@gmail.com>
2025-07-18 21:50:17 +0900

    [Refactor] #19: 필요 없는 import문 제거

--- src/main/java/com/example/BarrierKU/domain/building/service/BuildingService.java
+++ src/main/java/com/example/BarrierKU/domain/building/service/BuildingService.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.BarrierKU.common.response.ResponseCode.BUILDING_NOT_FOUND;
