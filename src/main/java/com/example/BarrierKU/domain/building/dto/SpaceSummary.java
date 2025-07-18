8b480b7fb5d348437e889d5af83e5b204476fb1a
choihongjun <vvclsbalam@gmail.com>
2025-07-18 18:03:58 +0900

    [Refactor] #19: SpaceSummary 수정

---
+++ src/main/java/com/example/BarrierKU/domain/building/dto/SpaceSummary.java
@@ -0,0 +1,20 @@
+package com.example.BarrierKU.domain.building.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+public record SpaceSummary (
+        @Schema(description = "공간 ID", example = "1")
+        Long id,
+        @Schema(description = "호수", example = "201")
+        String roomNumber,
+        @Schema(description = "호실명", example = "대강의실")
+        String roomName,
+        @Schema(description = "특이사항", example = "턱 있음")
+        String comment,
+        @Schema(description = "수업 여부", example = "true")
+        boolean isLecture,
+        List<RoomImageResponse> roomImages
+) {
+}
