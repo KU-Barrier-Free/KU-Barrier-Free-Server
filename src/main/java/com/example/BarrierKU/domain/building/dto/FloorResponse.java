ff5bc175235b67d69dd7cfd6c0c06dbac6ad60e0
choihongjun <vvclsbalam@gmail.com>
2025-07-22 23:25:13 +0900

    [Refactr] #19: FloorResponse SpaceSummary example 삭제

--- src/main/java/com/example/BarrierKU/domain/building/dto/FloorResponse.java
+++ src/main/java/com/example/BarrierKU/domain/building/dto/FloorResponse.java
@@ -10,15 +10,7 @@ public record FloorResponse(
         List<String> drawings,
         @Schema(description = "해당 층에 존재하는 편의시설 종류", example = "[\"은행\", \"휴게실\", \"카페\"]")
         Set<String> purposes,
-        @Schema(description = "공간의 요약된 정보",
-                example = "[{" +
-                        "\"id\": 1," +
-                        "\"roomNumber\": \"104-1호\"," +
-                        "\"roomName\": \"강의실\"," +
-                        "\"comment\": \"원형책상\"," +
-                        "\"roomImages\": [\"https://example.com/image1.jpg\", \"https://example.com/image2.jpg\"]," +
-                        "\"isLecture\": true" +
-                        "}]")
+        @Schema(description = "공간의 요약된 정보")
         List<SpaceSummary> spaceSummaries
 ) {
 }
