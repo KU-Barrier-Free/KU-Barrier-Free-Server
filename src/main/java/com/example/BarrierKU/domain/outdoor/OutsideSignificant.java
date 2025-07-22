0d883f458d67497bdf549b6c6f61ddeb0bbcdb4b
ksg <ksg001227@naver.com>
2025-07-22 21:35:46 +0900

    [Fix] #21 OutsideSignificant 에 잘못 표기된 어노테이션 수정

--- src/main/java/com/example/BarrierKU/domain/outdoor/OutsideSignificant.java
+++ src/main/java/com/example/BarrierKU/domain/outdoor/OutsideSignificant.java
@@ -2,8 +2,7 @@ package com.example.BarrierKU.domain.outdoor;
 
 import com.example.BarrierKU.domain.image.OutsideSignificantImage;
 import jakarta.persistence.*;
-import lombok.Generated;
-import lombok.Getter;
+import lombok.*;
 import org.locationtech.jts.geom.Point;
 
 import java.util.ArrayList;
@@ -13,7 +12,7 @@ import java.util.List;
 @Getter
 public class OutsideSignificant {
 
-    @Id @Generated
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "outside_significant_id")
     private Long id;
 
