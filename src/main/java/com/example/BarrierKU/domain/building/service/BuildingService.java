0fc7238ca9c3f01f70674ce47c654e8f740f0cb8
choihongjun <vvclsbalam@gmail.com>
2025-07-22 21:24:25 +0900

    [Feat] #19: 대소문자 구별하지 않고 검색 가능하게 변경

--- src/main/java/com/example/BarrierKU/domain/building/service/BuildingService.java
+++ src/main/java/com/example/BarrierKU/domain/building/service/BuildingService.java
@@ -40,7 +40,7 @@ public class BuildingService {
 
         List<SpaceSummary> spaces = building.getRooms().stream()
                 .filter(room ->
-                        room.getRoomName().contains(keyword) || room.getRoomNumber().contains(keyword)
+                        room.getRoomName().toLowerCase().contains(keyword.toLowerCase()) || room.getRoomNumber().contains(keyword)
                 )
                 .map(SpaceSummary::from)
                 .toList();
