809689346346445cf0320a8c6653a8cc25710e1f
ksg <ksg001227@naver.com>
2025-07-22 21:05:50 +0900

    [Feat] #21 교외 특이사항을 조회하면서 교외 특이사항 이미지까지 한 번에 조회하는 메서드 구현

--- src/main/java/com/example/BarrierKU/domain/home/dto/OutsideSignificantResponse.java
+++ src/main/java/com/example/BarrierKU/domain/home/dto/OutsideSignificantResponse.java
@@ -2,7 +2,7 @@ package com.example.BarrierKU.domain.home.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record OutdoorSignificantResponse(
+public record OutsideSignificantResponse(
         @Schema(description = "교외 특이사항 이미지", example = "http://image1.png")
         String imageUrl,
         @Schema(description = "교외 특이사항", example = "사진 기준 왼쪽에 경사로가 있어서 장애 학우들도 이용 가능합니다.")
