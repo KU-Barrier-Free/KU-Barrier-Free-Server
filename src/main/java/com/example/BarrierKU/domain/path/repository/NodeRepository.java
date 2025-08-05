bb81264840de1bd5831017aacb78cf6f4e3bcb5a
ksg <ksg001227@naver.com>
2025-08-05 23:54:26 +0900

    [Feat] #34 좌표 기반 조회를 위한 리포지토리 로직 작성

--- src/main/java/com/example/BarrierKU/domain/path/repository/NodeRepository.java
+++ src/main/java/com/example/BarrierKU/domain/path/repository/NodeRepository.java
@@ -9,6 +9,15 @@ import java.util.Optional;
 
 
 public interface NodeRepository extends JpaRepository<Node, Long> {
+
+    // 위경도로부터 가장 가까운 노드 찾기 (거리 단위: meter)
+    @Query(value = """
+        SELECT * 
+        FROM node 
+        ORDER BY ST_DistanceSphere(location, ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)) 
+        LIMIT 1
+        """, nativeQuery = true)
+    Optional<Node> findNearestNode(@Param("lat") double lat, @Param("lon") double lon);
 }
 
 
