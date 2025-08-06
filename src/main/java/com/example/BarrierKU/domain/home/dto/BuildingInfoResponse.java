4bc7c792298e3f65adaae9f2f74bd4f3d7bee7d1
yeonzeop <ksp2617@naver.com>
2025-08-06 11:56:23 +0900

    [Chore] #35 불필요 임포트 삭제

--- src/main/java/com/example/BarrierKU/domain/home/dto/BuildingInfoResponse.java
+++ src/main/java/com/example/BarrierKU/domain/home/dto/BuildingInfoResponse.java
@@ -1,17 +1,13 @@
 package com.example.BarrierKU.domain.home.dto;
 
-import com.example.BarrierKU.domain.building.dto.BuildingResponse;
 import com.example.BarrierKU.domain.building.dto.FloorResponse;
 import com.example.BarrierKU.domain.door.dto.DoorInfo;
-import com.example.BarrierKU.domain.image.DoorImage;
-import com.example.BarrierKU.domain.image.SignificantImage;
 import com.example.BarrierKU.domain.indoor.Door;
 import com.example.BarrierKU.domain.indoor.Significant;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
