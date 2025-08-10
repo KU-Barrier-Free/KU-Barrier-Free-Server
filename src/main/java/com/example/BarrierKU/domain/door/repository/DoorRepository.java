4497ab12eb68a0070b6f09d6efac4b6b8fb233e6
ksg <ksg001227@naver.com>
2025-08-10 23:56:49 +0900

    [Fix] #34 불필요한 세미콜론 제거

--- src/main/java/com/example/BarrierKU/domain/door/repository/DoorRepository.java
+++ src/main/java/com/example/BarrierKU/domain/door/repository/DoorRepository.java
@@ -28,7 +28,7 @@ public interface DoorRepository extends JpaRepository<Door, Long> {
             WHERE d.building_id = :buildingId
               AND d.wheelchair = true
             ORDER BY ST_DistanceSphere(d.spot, ST_SetSRID(ST_MakePoint(:lon, :lat), 4326))
-            LIMIT 1;
+            LIMIT 1
             """, nativeQuery = true)
     Optional<Point> findNearestWheelchairDoorSpot(@Param("buildingId") Long buildingId,
                                                   @Param("lat") double lat,
