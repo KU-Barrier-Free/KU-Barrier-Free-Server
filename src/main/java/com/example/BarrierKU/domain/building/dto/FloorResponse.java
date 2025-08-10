13429c5e4cc9d8701925f147c4c9c7e15d2765c9
yeonzeop <ksp2617@naver.com>
2025-08-10 10:32:22 +0900

    [Chore] 도면 Schema 예시 수정

--- src/main/java/com/example/BarrierKU/domain/building/dto/FloorResponse.java
+++ src/main/java/com/example/BarrierKU/domain/building/dto/FloorResponse.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 public record FloorResponse(
-        @Schema(description = "도면 이미지", example = "image.png")
+        @Schema(description = "도면 이미지", example = "[\"image.png\", \"image2.png\"]")
         List<String> drawings,
         @Schema(description = "해당 층에 존재하는 편의시설 종류", example = "[\"은행\", \"휴게실\", \"카페\"]")
         Set<String> purposes,
