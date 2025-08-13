61c031be40915f54ddedc5895f75ca37c2c0de1a
choihongjun <vvclsbalam@gmail.com>
2025-08-14 01:13:58 +0900

    [Feat] #40: 편의시설 조회 api swagger 명세 추가

--- src/main/java/com/example/BarrierKU/domain/facility/controller/FacilityController.java
+++ src/main/java/com/example/BarrierKU/domain/facility/controller/FacilityController.java
@@ -2,9 +2,10 @@ package com.example.BarrierKU.domain.facility.controller;
 
 import com.example.BarrierKU.common.annotation.CustomExceptionDescription;
 import com.example.BarrierKU.common.response.BaseResponse;
-import com.example.BarrierKU.common.swagger.SwaggerResponseDescription;
 import com.example.BarrierKU.domain.facility.dto.FacilitiesResponse;
 import com.example.BarrierKU.domain.facility.service.FacilityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 import static com.example.BarrierKU.common.swagger.SwaggerResponseDescription.*;
 
 @Slf4j
+@Tag(name = "Facility", description = "Facility API")
 @RequiredArgsConstructor
 @RequestMapping("/facilities")
 @RestController
 public class FacilityController {
     private final FacilityService facilityService;
 
+    @Operation(
+            summary = "편의시설 조회 API",
+            description = "편의시설 및 편의시설이 속한 건물 정보를 보여주기 위한 API 입니다."
+    )
     @GetMapping("/{facilityId}")
     @CustomExceptionDescription(GET_FACILITY)
     public BaseResponse<FacilitiesResponse> getFacility(@PathVariable Long facilityId) {
--- src/main/java/com/example/BarrierKU/domain/facility/dto/FacilitiesResponse.java
+++ src/main/java/com/example/BarrierKU/domain/facility/dto/FacilitiesResponse.java
@@ -1,12 +1,17 @@
 package com.example.BarrierKU.domain.facility.dto;
 
 import com.example.BarrierKU.domain.type.Purpose;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.locationtech.jts.geom.Point;
 
 public record FacilitiesResponse(
+        @Schema(description = "편의시설 ID", example = "1")
         Long facilityId,
+        @Schema(description = "편의시설 이름", example = "카페 레스티오")
         String facilityName,
+        @Schema(description = "편의시설 용도", example = "CAFE")
         String purpose,
+        @Schema(description = "건물 정보")
         BuildingInfo buildingInfo
 ) {
     public FacilitiesResponse(Long facilityId, String facilityName, Purpose purpose, Long buildingId, Point spot) {
@@ -14,8 +19,11 @@ public record FacilitiesResponse(
     }
 
     public record BuildingInfo(
+            @Schema(description = "건물 ID", example = "2")
             Long id,
+            @Schema(description = "위도", example = "37.541")
             double latitude,
+            @Schema(description = "경도", example = "127.078")
             double longitude
     ) {}
 }
