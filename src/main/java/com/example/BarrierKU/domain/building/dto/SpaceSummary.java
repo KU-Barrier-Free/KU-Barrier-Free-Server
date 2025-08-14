d6ece4e77091dd83a77f04bb0d800946b176c19c
choihongjun <vvclsbalam@gmail.com>
2025-08-15 02:42:20 +0900

    [Refactor] #40: SpaceSummary 특이사항에 계단 경사로 정보 추가

--- src/main/java/com/example/BarrierKU/domain/building/dto/SpaceSummary.java
+++ src/main/java/com/example/BarrierKU/domain/building/dto/SpaceSummary.java
@@ -33,11 +33,18 @@ public record SpaceSummary(
         boolean isLecture
 ) {
     public static SpaceSummary from(Room room) {
+        String comment = "";
+        if(room.getRoomInfo() != null) {
+            if(room.getRoomInfo().isStair()) comment += "계단 있음 ";
+            if(room.getRoomInfo().isRamp()) comment += "경사로 있음 ";
+            if(room.getRoomInfo().getRoomComment() != null) comment += room.getRoomInfo().getRoomComment();
+        }
+
         return new SpaceSummary(
                 room.getId(),
                 room.getRoomNumber() + "호",
                 room.getRoomName(),
-                room.getRoomInfo().getRoomComment(),
+                comment,
                 room.getRoomImages().stream()
                         .map(RoomImageResponse::from)
                         .toList(),
