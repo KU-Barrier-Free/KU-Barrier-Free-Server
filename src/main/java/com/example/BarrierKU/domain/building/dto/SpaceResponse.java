b3a28042ed7a4c04d8c7a0c4da7975a395cbd24b
choihongjun <vvclsbalam@gmail.com>
2025-08-18 15:58:32 +0900

    [Refactor] #40: SpaceResponse 관리부서 getter 수정

--- src/main/java/com/example/BarrierKU/domain/building/dto/SpaceResponse.java
+++ src/main/java/com/example/BarrierKU/domain/building/dto/SpaceResponse.java
@@ -70,8 +70,8 @@ public class SpaceResponse {
                 comment.trim(),
                 room.getFloorSpace(),
                 room.getRoomType().getValue(),
-                room.getDepartment(),
-                room.getDepartmentNumber(),
+                room.getDepartment().getName(),
+                room.getDepartment().getDepartmentNumbers().getFirst().getNumber(),
                 info,
                 images
         );
