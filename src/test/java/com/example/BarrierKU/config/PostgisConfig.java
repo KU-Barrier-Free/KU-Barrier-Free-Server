cb62c8ed847ab78da9bb106523767c418e52b993
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-09-12 20:40:09 +0900

    [Fix] #51 flyway 설정 제거

--- src/test/java/com/example/BarrierKU/config/PostgisConfig.java
+++ src/test/java/com/example/BarrierKU/config/PostgisConfig.java
@@ -3,7 +3,6 @@ package com.example.BarrierKU.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
