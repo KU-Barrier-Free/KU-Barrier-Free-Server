a18bf54b07faf65abf36ac288d3f8eeee86d5302
choihongjun <vvclsbalam@gmail.com>
2025-11-23 11:14:22 +0900

    [Refactor] #62: significantImageRepository 삭제

--- src/main/java/com/example/BarrierKU/domain/significantImage/repository/SignificantImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BarrierKU.domain.significantImage.repository;
-
-import com.example.BarrierKU.domain.image.SignificantImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SignificantImageRepository extends JpaRepository<SignificantImage, Long> {
-}
