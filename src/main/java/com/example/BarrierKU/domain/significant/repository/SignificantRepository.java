d35e592403c83713cfddfc38c2ab6e882cf08b99
choihongjun <vvclsbalam@gmail.com>
2025-11-23 11:13:53 +0900

    [Refactor] #62: significantRepository 삭제

--- src/main/java/com/example/BarrierKU/domain/significant/repository/SignificantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BarrierKU.domain.significant.repository;
-
-import com.example.BarrierKU.domain.indoor.Significant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SignificantRepository extends JpaRepository<Significant, Long> {
-}
