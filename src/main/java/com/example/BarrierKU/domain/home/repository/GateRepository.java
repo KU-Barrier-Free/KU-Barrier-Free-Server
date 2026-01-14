7042000c236443ed1b21532028163264e92f59cf
choihongjun <vvclsbalam@gmail.com>
2026-01-14 23:15:33 +0900

    [Feat] #65: GateRepository 추가

---
+++ src/main/java/com/example/BarrierKU/domain/home/repository/GateRepository.java
@@ -0,0 +1,7 @@
+package com.example.BarrierKU.domain.home.repository;
+
+import com.example.BarrierKU.domain.outdoor.Gate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GateRepository extends JpaRepository<Gate, Long> {
+}
